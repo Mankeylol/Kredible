71d7c0ef0b6e3110444cc0905382f692ccadd9ca
mankeylol <mankeyooooo@gmail.com>
2023-04-13 17:44:53 +0530

    near completion

--- Credable-backend/index.js
+++ Credable-backend/index.js
@@ -1,7 +1,16 @@
 import puppeteer from "puppeteer"
+import * as dotenv from 'dotenv';
 import { exec } from "node:child_process"
 import { promisify } from "node:util"
+import { Metaplex, toMetaplexFile,bundlrStorage,
+    keypairIdentity, } from "@metaplex-foundation/js";
+import { Connection, clusterApiUrl, Keypair } from "@solana/web3.js";
+import fs from "fs"
+import express from 'express'
 
+const app = express()
+const PORT = 8080;
+app.use(express.json())
 
 
 const { stdout: chromiumPath } = await promisify(exec)("which chromium")
@@ -58,18 +67,61 @@ function generateHtml(userName, score) {
     `;
 }
 
-async function main() {
-    const [, , userName, score, imageUrl] = process.argv;
-    const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'], executablePath: chromiumPath.trim() });
+async function mintNftController( userName, score) {
+   const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'], executablePath: chromiumPath.trim() });
     const page = await browser.newPage();
     await page.setViewport({
         width: 480,
         height: 760,
         deviceScaleFactor: 1,
     });
-    await page.setContent(generateHtml(userName, score, imageUrl));
+    await page.setContent(generateHtml(userName, score));
     await page.screenshot({ path: `./${userName}.png` });
     await browser.close();
+
+   
+
+    const keypair = Keypair.fromSecretKey(
+        new Uint8Array([228,252,71,40,185,159,47,71,216,100,105,210,198,177,136,23,156,85,116,180,244,136,78,111,147,165,42,19,142,251,166,57,156,134,203,201,6,226,141,186,150,111,175,216,215,12,187,37,64,89,89,183,176,30,145,221,62,200,119,134,66,33,135,43])
+    );
+    console.log(keypair.publicKey)
+
+    const connection = new Connection(clusterApiUrl("devnet"));
+    const metaplex = Metaplex.make(connection).use(keypairIdentity(keypair)).use(bundlrStorage({
+        address: 'https://devnet.bundlr.network',
+        providerUrl: 'https://api.devnet.solana.com',
+        timeout: 60000,
+    }))
+    const imageBuffer = fs.readFileSync(`/Users/mayankjha/Credable/Credable-backend/${userName}.png`);
+    const file = toMetaplexFile(imageBuffer, "mankey.png");
+
+    const { uri, metadata } = await metaplex.nfts().uploadMetadata({
+        name: "Kredible Score NFT",
+        image: file,
+    },
+    { commitment: "finalized" }
+    );
+    
+    console.log(metadata.image) // https://arweave.net/123
+    console.log(uri)
+
+   
+        const { nft } = await metaplex.nfts().create(
+            {
+                uri: uri,
+                name: "Kredible score",
+                sellerFeeBasisPoints: 500, // Represents 5.00%.
+                maxSupply: 1,
+            },
+            { commitment: "finalized" }
+        );
+        console.log(nft);
 }
 
-main();
+app.post('/mintnft', (request, response)=>{
+    const {userName, score} = request.body
+    mintNftController(userName, score)
+    response.status(200).send("POST request received")
+})
+
+app.listen(PORT, () =>console.log("da local host is alive"))
--- credable-frontend/src/components/MintNFT.tsx
+++ credable-frontend/src/components/MintNFT.tsx
@@ -4,13 +4,29 @@ import * as buffer from "buffer";
 
 window.Buffer = buffer.Buffer;
 
-export default function App () {
+export default function App() {
 
-    
+  function handleClick() {
+    const url = 'http://localhost:8080/mintnft';
+    const data = {
+      userName: 'John dee',
+      score: 331
+    };
+
+    fetch(url, {
+      mode: 'no-cors',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+  }
+
 
   return (
     <div>
-        <button className='getDataBtn'> Mint NFT</button>
+      <button onClick={handleClick} className='getDataBtn'> Mint NFT</button>
     </div>
   );
 }
