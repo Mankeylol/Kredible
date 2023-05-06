9645b1b9cfdadfa85c3a79b1e1e382e24d0868e7
mankeylol <mankeyooooo@gmail.com>
2023-05-06 22:52:34 +0530

    made some changes

--- Credable-backend/index.js
+++ Credable-backend/index.js
@@ -1,13 +1,16 @@
 import puppeteer from "puppeteer"
-import * as dotenv from 'dotenv';
 import { exec } from "node:child_process"
 import { promisify } from "node:util"
 import { Metaplex, toMetaplexFile,bundlrStorage,
     keypairIdentity, } from "@metaplex-foundation/js";
-import { Connection, clusterApiUrl, Keypair } from "@solana/web3.js";
+import { Connection, clusterApiUrl, Keypair, LAMPORTS_PER_SOL,} from "@solana/web3.js";
+import solanaWeb3 from '@solana/web3.js';
 import fs from "fs"
 import express from 'express'
 import cors from 'cors'
+import axios from "axios";
+import { totalmem } from "node:os";
+
 
 const app = express()
 const PORT = 8080;
@@ -119,8 +122,223 @@ async function mintNftController( request, response) {
         );
         console.log(nft);
 }
+
+async function calculateScore(request, response) {
+    /* let floorPrice = 0
+    let nftValue = 0
+    let totalTxs = 0
+    const walletAddress = "5PTzRVufPqm3M3xTxq2AAkPhq1XxhmrW6LgQBunxJFvS" /*request.body
+
+
+    //Gets all the Nfts in the 
+    const getNfts = async () => {
+
+        let totalFloor = 0
+
+        const options = {
+            method: 'POST',
+            url: 'https://rest-api.hellomoon.io/v0/nft/mints-by-owner',
+            headers: {
+              accept: 'application/json',
+              'content-type': 'application/json',
+              authorization:   'Bearer f9322af6-68dd-4cce-a894-76bf23a0a005'
+            },
+            data: { ownerAccount: walletAddress }
+          };
+
+        const nfts = await axios.request(options);
+        const nftData = nfts.data
+
+        async function getTotalFloor() {
+
+            
+            const Nft_array_length = nftData.data.length
+
+            try {
+                for (let i = 0; i < 1; i++) {
+                  let collectionId = nftData.data[i].helloMoonCollectionId;  
+                  console.log(collectionId)
+                  const options = {
+                    method: 'POST',
+                    url: 'https://rest-api.hellomoon.io/v0/nft/collection/floorprice',
+                    headers: {
+                      accept: 'application/json',
+                      'content-type': 'application/json',
+                      authorization: 'Bearer f9322af6-68dd-4cce-a894-76bf23a0a005'
+                    },
+                    data: { helloMoonCollectionId: collectionId }
+                  };
+                  if (collectionId) {
+                    const floorPrice = await axios.request(options);
+                  const nftValue = floorPrice.data.data[0].floorPriceLamports / LAMPORTS_PER_SOL;
+                  console.log(nftValue);
+                  totalFloor += nftValue; 
+                  } else{
+                    totalFloor += 0; 
+                  }
+                }
+                return totalFloor;
+              } catch (error) {
+                console.error(error);
+              }
+            
+        } 
+
+        floorPrice = await getTotalFloor()
+
+        return floorPrice
+    }
+    nftValue = await getNfts()
+    console.log(nftValue)
+    
+
+    const getTransactions = async () => {
+        const endpoint = "https://solana-mainnet.g.alchemy.com/v2/FG8gabHTRpZujrxEb_rQKlldhhXftzlK" ;
+        const solanaConnection = new solanaWeb3.Connection(endpoint);
+        const pubKey = new solanaWeb3.PublicKey(walletAddress);
+
+        let transactionList = await solanaConnection.getSignaturesForAddress(pubKey,);``
+        return transactionList
+      }
+      totalTxs = await getTransactions()
+      console.log(totalTxs)
+      console.log(totalTxs.length)
+
+      const getAllTokens = async () => {
+
+        const options = {
+          method: 'POST',
+          url: 'https://rest-api.hellomoon.io/v0/token/balances-by-owner',
+          headers: {
+            accept: 'application/json',
+            'content-type': 'application/json',
+            authorization:   'Bearer f9322af6-68dd-4cce-a894-76bf23a0a005'
+          },
+          data: { ownerAccount: walletAddress }
+        }
+        const tokenList = await axios.request(options);
+        const len = tokenList.data.length
+        console.log(len)
+        
+        return tokenList
+      }
+      let allTokens = await getAllTokens()
+    
+      async function getFungibleTokens() {
+        const getNfts = async () => {
+
+    
+            const options = {
+                method: 'POST',
+                url: 'https://rest-api.hellomoon.io/v0/nft/mints-by-owner',
+                headers: {
+                  accept: 'application/json',
+                  'content-type': 'application/json',
+                  authorization:   'Bearer f9322af6-68dd-4cce-a894-76bf23a0a005'
+                },
+                data: { ownerAccount: walletAddress }
+              };
+    
+            const nfts = await axios.request(options);
+            const nftData = nfts.data.data
+
+            console.log(nftData.length)
+
+            return nftData
+        }
+
+        const nftData = await getNfts()
+
+        for (let i = 0; i < nftData.length; i++) {
+          let aObj = nftData[i];
+
+          for (let j = 0; j < allTokens.data.length; j++){
+            if (allTokens.data[j].mint === aObj.nftMint) {
+                allTokens.data.splice(j,1)
+                break;
+            }
+          }
+        }
+      
+        return allTokens;
+      }
+
+      let tokens = await getFungibleTokens()
+      console.log(tokens.data.length)
+
+      async function getTokenValue(){
+
+        let totalvalue = 0
+
+        try {
+            for (let i = 0; i <  tokens.data.length; i++) {
+                let mintAddress = tokens.data[i].mint
+                console.log(mintAddress)
+                const options = {
+                    method: 'POST',
+                    url: 'https://rest-api.hellomoon.io/v0/token/price',
+                    headers: {
+                      accept: 'application/json',
+                      'content-type': 'application/json',
+                      authorization:   'Bearer f9322af6-68dd-4cce-a894-76bf23a0a005'
+                    },
+                    data: { mint: mintAddress }
+                  };
+                
+                  const tokenPrice  = await axios.request(options);
+
+                  if (tokenPrice.data.data[0]) {
+                    if (mintAddress === "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v" || mintAddress == "Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB" ) {
+                        console.log(tokens.data[i].amount)
+                        totalvalue += tokens.data[i].amount / 1000000
+                    }
+                    totalvalue += (tokenPrice.data.data[0].price / 1000000000000000) * tokens.data[i].amount
+                    console.log(totalvalue)
+                  } else {
+                    totalvalue += 0
+                  }
+
+
+
+            }
+        } catch (error) {
+            console.log(error)
+        } 
+
+        console.log(totalvalue)
+
+        return totalvalue
+      }
+
+      getTokenValue() */
+
+      async function getStakeAccountInfo(){
+        const options = {
+            method: 'POST',
+            url: 'https://rest-api.hellomoon.io/v0/defi/staking/accounts',
+            headers: {
+              accept: 'application/json',
+              'content-type': 'application/json',
+              authorization:   'Bearer f9322af6-68dd-4cce-a894-76bf23a0a005'
+            },
+            data: { stakeAuthority: "2zyA1kf28Dh8sv3pLJ47ioBygC4tP95x9hNYMzpiay4n" }
+          };
+        
+          const stakeAccounts  = await axios.request(options)
+          console.log(stakeAccounts.data.data)
+      }
+
+      getStakeAccountInfo()
+}
+calculateScore()
+
+
+
 app.use(cors())
 app.use(express.json())
+
+app.post('/calculateScore',
+calculateScore)
 
 app.post('/mintnft',
     mintNftController)
--- credable-frontend/src/components/GetScore.tsx
+++ credable-frontend/src/components/GetScore.tsx
@@ -2,23 +2,18 @@ import { useWallet, } from '@solana/wallet-adapter-react';
 import React, { useEffect, useState } from 'react';
 import { NftSVG } from './NftSVG';
 import axios from 'axios'
-import { LAMPORTS_PER_SOL, Connection, PublicKey, StakeProgram, ParsedInstruction } from '@solana/web3.js';
+import { Connection, PublicKey, StakeProgram, ParsedInstruction } from '@solana/web3.js';
 import MintNFT from './MintNFT'
-import * as dotenv from 'dotenv';
 
 
-const solanaWeb3 = require('@solana/web3.js');
+
+
 const endpoint = "https://solana-mainnet.g.alchemy.com/v2/FG8gabHTRpZujrxEb_rQKlldhhXftzlK" ;
-const solanaConnection = new solanaWeb3.Connection(endpoint);
-let totalNftValue: number = 0
 
 export default function GetScore() {
 
   const [visibility, setVisibility] = useState(true)
   const [tokenListArray, setTokenArray] = useState<any[]>([])
-  const [totalTransactions, setTotalTransactions] = useState<any[]>([])
-  const [nftArray, setNftArray] = useState<any[]>([])
-  const [nftValue, setNftsValue] = useState<any>()
   const [score, setScore] = useState(0)
   const { publicKey, } = useWallet();
   const walletAddress: string = publicKey?.toBase58()!
@@ -32,81 +27,6 @@ export default function GetScore() {
     
   };
   function getScore() {
-    const getTransactions = async () => {
-      const pubKey = new solanaWeb3.PublicKey(walletAddress);
-      let transactionList = await solanaConnection.getSignaturesForAddress(pubKey,);
-      console.log(transactionList)
-      setTotalTransactions(transactionList)
-    }
-    getTransactions()
-
-    const getNftFloor = async () => {
-
-      const options = {
-        method: 'POST',
-        url: 'https://rest-api.hellomoon.io/v0/nft/mints-by-owner',
-        headers: {
-          accept: 'application/json',
-          'content-type': 'application/json',
-          authorization:   'Bearer f9322af6-68dd-4cce-a894-76bf23a0a005'
-        },
-        data: { ownerAccount: walletAddress }
-      };
-
-      const nfts = await axios.request(options);
-      const nftData = nfts.data
-      const nftLen = nftData.data.length
-      setNftArray(nftData.data)
-
-      for (let i = 0; i < 3; i++) {
-        const collectionId = nftData.data[i].helloMoonCollectionId
-        const nftFloor = async () => {
-
-          const options = {
-            method: 'POST',
-            url: 'https://rest-api.hellomoon.io/v0/nft/collection/floorprice',
-            headers: {
-              accept: 'application/json',
-              'content-type': 'application/json',
-              authorization:   'Bearer f9322af6-68dd-4cce-a894-76bf23a0a005'
-            },
-            data: { helloMoonCollectionId: collectionId }
-          }
-          const floorPrice = await axios.request(options);
-          const nftValue = floorPrice.data.data[0].floorPriceLamports / LAMPORTS_PER_SOL
-          console.log(nftValue)
-          totalNftValue += nftValue
-          console.log(totalNftValue)
-          setNftsValue(totalNftValue)
-        }
-        nftFloor()
-
-      }
-    };
-    getNftFloor()
-
-    function getTokenPrices() {
-      const getAllTokens = async () => {
-
-        const options = {
-          method: 'POST',
-          url: 'https://rest-api.hellomoon.io/v0/token/balances-by-owner',
-          headers: {
-            accept: 'application/json',
-            'content-type': 'application/json',
-            authorization:   'Bearer f9322af6-68dd-4cce-a894-76bf23a0a005'
-          },
-          data: { ownerAccount: walletAddress }
-        }
-        const tokenList = await axios.request(options);
-        console.log(tokenList)
-        setTokenArray(tokenList.data)
-        const len = tokenList.data.length
-        console.log(len)
-      }
-      getAllTokens()
-    }
-    getTokenPrices()
 
     async function main() {
 
@@ -140,41 +60,16 @@ export default function GetScore() {
     
       for (const stakeAccount of stakeAccounts) {
         console.log("stakeAccount:", stakeAccount.pubkey.toBase58());
-    
-        // get transactions related to stakeAccount
-        const signatureInfos = await connection.getSignaturesForAddress(stakeAccount.pubkey); // TODO: pagenation if many it has many transactions
-        const signatures = signatureInfos.map((signatureInfo) => signatureInfo.signature);
-        const transactions = await connection.getParsedTransactions(signatures, {maxSupportedTransactionVersion: 0});
-        console.log(transactions)
-    
-        // print StakeProgram instructions included in the transactions
-        transactions.forEach((tx) => {
-          console.log("\ttransaction:", tx!.transaction.signatures[0], "slot", tx!.slot, "blockTime", new Date(tx!.blockTime! * 1000).toISOString());
-          tx!.transaction.message.instructions.forEach((instruction) => {
-            if (instruction.programId.equals(StakeProgram.programId)) {
-              const parsed = instruction as ParsedInstruction;
-              console.log("\t\tinstruction:", parsed.program, parsed.parsed.type);
-            }
-          });
-        });
       }
     }
     
     main();
   }
-  console.log(nftValue)
-  console.log(nftArray)
   console.log(tokenListArray)
   function mintNft(){
     getScore()
     setVisibility(!visibility)
   }
-  useEffect(()=>{
-      function calculateScore (){
-        setScore(totalTransactions.length)
-      }
-      calculateScore()
-  }, [totalTransactions])
 
   return (
     <div className='credit-score-container'>
